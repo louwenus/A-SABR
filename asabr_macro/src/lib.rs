f1ee43d445bda8535f1b2a0bf90813a265ba50cd
Mwa <mwa@mwaaa.fr>
2026-06-10 14:06:18 +0200

    dummy but legal crate to pass CI

--- asabr_macro/src/lib.rs
+++ asabr_macro/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! Proc macro crate, Not Yet Implemented
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = 2 + 2;
         assert_eq!(result, 4);
     }
 }
